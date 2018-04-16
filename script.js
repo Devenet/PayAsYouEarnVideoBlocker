219eeb5a730ad824caaa8399b4bcd73dd6b43cdb
Nicolas Devenet <nicolas@devenet.info>
2018-04-16 17:19:16 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var cookieName = 'videoPAS_session';
+var cookieValue = 'nepasreafficher';
+
+window.getCookie = function(name) {
+  match = document.cookie.match(new RegExp(name + '=([^;]+)'));
+  if (match) return match[1];
+}
+
+if (!getCookie(cookieName)) {
+	var expiration = (new Date()).getTime() + 31;
+	document.cookie = cookieName +'='+ cookieValue +'; expires='+ expiration+'; path=/';
+	document.getElementById('prehome-modal').innerHTML = '';
+}
